11e6986ff69f92d6683c61f10e2e46e7ba3a645f
zengpeng <1769411197@qq.com>
2019-11-28 17:52:03 +0800

    提交：去除多余import;加上集群broker地址

--- src/main/java/com/hjmos/springbootrocketmq/controller/TestRocketMQSendController.java
+++ src/main/java/com/hjmos/springbootrocketmq/controller/TestRocketMQSendController.java
@@ -6,12 +6,8 @@ import com.hjmos.springbootrocketmq.entity.RocketProduceMessage;
 import com.hjmos.springbootrocketmq.service.KafkaProduceMessageService;
 import com.hjmos.springbootrocketmq.service.ProduceMessageService;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
