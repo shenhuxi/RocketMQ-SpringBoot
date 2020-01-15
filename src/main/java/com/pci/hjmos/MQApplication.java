4c9c21862ed68071ef71e9efdeb14d3d85b96345
zhouyunting <jason@163.com>
2020-01-15 11:40:31 +0800

    update

--- src/main/java/com/pci/hjmos/MQApplication.java
+++ src/main/java/com/pci/hjmos/MQApplication.java
@@ -3,16 +3,11 @@ package com.pci.hjmos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author yuyang
- * 程序入口
- *
- */
 @SpringBootApplication
 public class MQApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MQApplication.class, args);
+        SpringApplication.run(MQApplication.class,args);
     }
 
 }
