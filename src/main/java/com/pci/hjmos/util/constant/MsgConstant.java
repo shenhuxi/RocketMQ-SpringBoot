b6deb201047a21a1fdc6562d0bd768e761937aa3
zhouyunting <jason@163.com>
2020-01-15 11:40:53 +0800

    操作提示信息

---
+++ src/main/java/com/pci/hjmos/util/constant/MsgConstant.java
@@ -0,0 +1,16 @@
+package com.pci.hjmos.util.constant;
+
+public class MsgConstant {
+
+    /**
+     * 操作成功
+     */
+    public final static String SUCCESS = "success";
+
+    /**
+     * 操作失败
+     */
+    public final static String ERROR = "error";
+
+
+}
