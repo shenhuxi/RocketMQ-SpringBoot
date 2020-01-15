d442b8f937248754d0e81c488adaee42a42d2c2e
zhouyunting <jason@163.com>
2020-01-15 11:41:25 +0800

    update

--- src/main/java/com/pci/hjmos/api/produce/ProduceMessageService.java
+++ src/main/java/com/pci/hjmos/api/produce/ProduceMessageService.java
@@ -2,17 +2,19 @@ package com.pci.hjmos.api.produce;
 
 import com.pci.hjmos.util.entity.Result;
 import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.springframework.stereotype.Component;
 
 /**
  * 生产消息的服务接口
  * @author zengpeng
  */
+@Component
 public interface ProduceMessageService {
     /**
      * 发送同步消息
      * @param topic 消息主题
      * @param content 消息内容
-     * @return 消息发送结果
      */
     Result sendSyncMsg(String topic, String content) throws Exception;
 
