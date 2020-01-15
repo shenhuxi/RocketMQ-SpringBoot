82ae0366dff3aa33944add8bdfa708f9d6982fc1
zhouyunting <jason@163.com>
2020-01-15 11:44:17 +0800

    测试发送消息

--- src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
+++ src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
@@ -1,9 +1,10 @@
 package com.pci.hjmos.controller;
 
 import com.pci.hjmos.api.produce.ProduceMessageService;
+import com.pci.hjmos.util.entity.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/TestRocketMQSend")
 public class TestRocketMQSendController {
+
+//    @Autowired
+//    private ProduceMessageService kafkaProduceMessageServiceImpl;
+
     @Autowired
-    private ProduceMessageService kafkaProduceMessageServiceImpl;
+    private ProduceMessageService produceMessageService;
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() throws Exception {
+
+        log.info("发送一条消息...........");
+        String content = "测试一下";
+        Result res = produceMessageService.sendSyncMsg("my-topic", content);
+
+        return "发送一条消息："+content;
+    }
 
 }
 
