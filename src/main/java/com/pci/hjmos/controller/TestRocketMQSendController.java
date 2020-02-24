a7017908fc8298e0e061e1923006fe07044289ed
zhouyunting <jason@163.com>
2020-02-24 14:22:23 +0800

    测试接口发送消息

--- src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
+++ src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
@@ -1,19 +1,23 @@
 package com.pci.hjmos.controller;
 
+import com.pci.hjmos.api.produce.MQCallback;
 import com.pci.hjmos.api.produce.ProduceMessageService;
 import com.pci.hjmos.util.entity.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.callback.Callback;
 
 /**
  * 本类只用于测试服务，实际不存在
  */
 @RestController()
 @Slf4j
-@RequestMapping("/TestRocketMQSend")
+//@RequestMapping("/TestRocketMQSend")
 public class TestRocketMQSendController {
 
 //    @Autowired
@@ -31,10 +35,22 @@ public class TestRocketMQSendController {
     public String sendMessage() throws Exception {
 
         log.info("发送一条消息...........");
-        String content = "测试一下";
-        Result res = produceMessageService.sendSyncMsg("my-topic", content);
+        String content = "hhhh";
+        //发送同步消息
+//        Result res = produceMessageService.sendSyncMsg("my-topic", content);
+        // 发送异步消息
+        produceMessageService.sendAsyncMsg("my-topic", content, new MQCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                System.out.println("发送成功");
+            }
+            @Override
+            public void onException(Throwable e) {
+                System.out.println("发送失败：" + e);
+            }
+        });
 
-        return "发送一条消息："+content;
+        return "发送一条异步消息："+content;
     }
 
 }
