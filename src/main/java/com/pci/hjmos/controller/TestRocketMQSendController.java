cb1c59065b688e1ea6819f96bccc91c951b92774
zhouyunting <jason@163.com>
2020-02-24 16:01:22 +0800

    测试发送消息案例

--- src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
+++ src/main/java/com/pci/hjmos/controller/TestRocketMQSendController.java
@@ -53,12 +53,63 @@ public class TestRocketMQSendController {
         return "发送一条异步消息："+content;
     }
 
+    @GetMapping("/sendSyncMsg")
+    public String sendSyncMsg(String topic,String content) throws Exception {
+
+        log.info("发送一条消息...........");
+        // 发送同步消息
+        Result result = produceMessageService.sendSyncMsg(topic, content);
+
+        return "发送一条同步消息："+content;
+    }
+
+    /**
+     * 发送异步消息测试
+     * @param topic   消息主题
+     * @param content 消息内容
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/sendAsyncMsg")
+    public String sendAsyncMsg(String topic,String content) throws Exception {
+
+        // 发送异步消息
+        produceMessageService.sendAsyncMsg(topic, content, new MQCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                log.info("发送异步消息成功，处理下一步业务");
+            }
+            @Override
+            public void onException(Throwable e) {
+                log.info("发送异步消息失败，抛出异常："+ e);
+            }
+        });
+
+        return "发送一条异步消息："+content;
+    }
+
+    /**
+     * 发送一条单向消息
+     * @param topic
+     * @param content
+     * @return
+     */
+    @GetMapping("/sendOneWayMsg")
+    public String sendOneWayMsg(String topic,String content) {
+
+        log.info("发送一条消息...........");
+        // 发送同步消息
+        produceMessageService.sendOneWayMsg(topic, content);
+
+        return "发送一条单向消息："+content;
+    }
+
 }
 
 /**
  * 仅用于测试使用
  */
-class User {
+/*class User {
     private String name;
     private int age;
 
@@ -82,4 +133,4 @@ class User {
     public void setAge(int age) {
         this.age = age;
     }
-}
+}*/
